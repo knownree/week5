e03085dedb4ad5d8f740b87c3be26170667a80d2
knownree <108420849+knownree@users.noreply.github.com>
2022-07-31 13:29:03 +0100

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+cab = pd.read_csv('Cab.csv')
+
+x=cab[['KM Travelled', 'Price Charged']]
+y=cab['Cost of Trip']
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3, random_state=101)
+
+lm=LinearRegression()
+lm.fit(x_train,y_train)
+
+pickle.dump(lm, open('model.pkl', 'wb'))
